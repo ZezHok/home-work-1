d7d70a81af7462d4fbf80b6e5183d77b3cee6149
Yulia Chistova <yulia_1992@mail.ru>
2016-02-29 23:28:47 +0300

    Некоторые доработки для задания 5.1

--- addressbook-web-test/src/test/java/ru/stqa/homework/addressbook/TestBase.java
+++ addressbook-web-test/src/test/java/ru/stqa/homework/addressbook/TestBase.java
@@ -81,4 +81,53 @@ public class TestBase {
   protected void selectedGroup() {
     wd.findElement(By.name("selected[]")).click();
   }
+
+  protected void gotoHomePage() {
+    wd.findElement(By.linkText("home")).click();
+  }
+
+  protected void submitContactCreation() {
+    wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
+  }
+
+  protected void fillContactForm(ContactData contactData) {
+    wd.findElement(By.name("firstname")).click();
+    wd.findElement(By.name("firstname")).clear();
+    wd.findElement(By.name("firstname")).sendKeys(contactData.getFirstName());
+    wd.findElement(By.name("middlename")).click();
+    wd.findElement(By.name("middlename")).clear();
+    wd.findElement(By.name("middlename")).sendKeys(contactData.getMiddleName());
+    wd.findElement(By.name("lastname")).click();
+    wd.findElement(By.name("lastname")).clear();
+    wd.findElement(By.name("lastname")).sendKeys(contactData.getLastName());
+    wd.findElement(By.name("mobile")).click();
+    wd.findElement(By.name("mobile")).clear();
+    wd.findElement(By.name("mobile")).sendKeys(contactData.getMobileNumber());
+    wd.findElement(By.name("email")).click();
+    wd.findElement(By.name("email")).click();
+    wd.findElement(By.name("email")).clear();
+    wd.findElement(By.name("email")).sendKeys(contactData.getEmail());
+  }
+
+  protected void gotoAddNewContactPage() {
+    wd.findElement(By.linkText("add new")).click();
+  }
+
+  private void login(String username, String password) {
+    wd.findElement(By.name("user")).click();
+    wd.findElement(By.name("user")).clear();
+    wd.findElement(By.name("user")).sendKeys(username);
+    wd.findElement(By.name("pass")).click();
+    wd.findElement(By.name("pass")).clear();
+    wd.findElement(By.name("pass")).sendKeys(password);
+    wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
+  }
+
+  protected void deleteContact() {
+      wd.findElement(By.xpath("//div[@id='content']/form[2]/input[2]")).click();
+  }
+
+  protected void editContact() {
+      wd.findElement(By.xpath("//table[@id='maintable']/tbody/tr[2]/td[8]/a/img")).click();
+  }
 }
