bceaf30d286b1838ee91dd003fbd8355bb416d7a
Yulia Chistova <yulia_1992@mail.ru>
2016-03-28 21:04:36 +0300

    Закрываем  Writer и Reader после выполнения

--- addressbook-web-test/src/test/java/ru/stqa/homework/addressbook/generators/GroupDataGenerator.java
+++ addressbook-web-test/src/test/java/ru/stqa/homework/addressbook/generators/GroupDataGenerator.java
@@ -20,13 +20,13 @@ import java.util.List;
  */
 public class GroupDataGenerator {
 
-  @Parameter (names = "-c", description = "Group count")
-  public  int count;
+  @Parameter(names = "-c", description = "Group count")
+  public int count;
 
-  @Parameter (names = "-f", description = "Target file")
+  @Parameter(names = "-f", description = "Target file")
   public String file;
 
-  @Parameter (names = "-d", description = "Data format")
+  @Parameter(names = "-d", description = "Data format")
   public String format;
 
   public static void main(String[] args) throws IOException {
@@ -34,7 +34,7 @@ public class GroupDataGenerator {
     JCommander jCommander = new JCommander(generator);
     try {
       jCommander.parse(args); // args - те опции которые переданны в командной строке
-    } catch (ParameterException ex){
+    } catch (ParameterException ex) {
       jCommander.usage();
       return;
     }
@@ -45,11 +45,11 @@ public class GroupDataGenerator {
     List<GroupData> groups = generateGroups(count);
     if (format.equals("csv")) {
       saveAsCsv(groups, new File(file));
-    } else if (format.equals("xml")){
+    } else if (format.equals("xml")) {
       saveAsXml(groups, new File(file));
-    }  else if (format.equals("json")){
+    } else if (format.equals("json")) {
       saveAsJson(groups, new File(file));
-    }else {
+    } else {
       System.out.println("Unrecognized format" + format);
     }
   }
@@ -57,28 +57,30 @@ public class GroupDataGenerator {
   private void saveAsJson(List<GroupData> groups, File file) throws IOException {
     Gson gson = new GsonBuilder().setPrettyPrinting().excludeFieldsWithoutExposeAnnotation().create();
     String json = gson.toJson(groups);
-    Writer writer = new FileWriter(file);
-    writer.write(json);
-    writer.close();
+    try (Writer writer = new FileWriter(file)) {
+      writer.write(json);
+    }
   }
 
   private void saveAsXml(List<GroupData> groups, File file) throws IOException {
     XStream xstream = new XStream();
     xstream.processAnnotations(GroupData.class); // внести изменения в груп дата
     String xml = xstream.toXML(groups);
-    Writer writer = new FileWriter(file);
-    writer.write(xml);
-    writer.close();
+    try (Writer writer = new FileWriter(file)) {
+      writer.write(xml);
+    }
   }
 
 
   private void saveAsCsv(List<GroupData> groups, File file) throws IOException { //список который сгенерили generateGroups сохраняем в файл
-    Writer writer = new FileWriter(file);                       //открываем файл на запись
-    for (GroupData group : groups) {                           //проходимся в цикле по всем группам и каждую из них записываем
-      writer.write(String.format("%s;%s;%s\n", group.getName(), group.getHeader(), group.getFooter()));
+    try (Writer writer = new FileWriter(file)) {                      //открываем файл на запись
+      for (GroupData group : groups) {                           //проходимся в цикле по всем группам и каждую из них записываем
+        writer.write(String.format("%s;%s;%s\n", group.getName(), group.getHeader(), group.getFooter()));
+      }
     }
-    writer.close();                                             //закрыть файл
   }
+
+
 
   private List<GroupData> generateGroups(int count) {    //генерим тестовые данные
     List<GroupData> groups = new ArrayList<GroupData>();
