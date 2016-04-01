152b3ddfbabefbcbf7df2007259fea39752b88fe
Yulia Chistova <yulia_1992@mail.ru>
2016-04-02 00:09:52 +0300

    Урок 7 Лекция 2 Object Relational Mapping
    Реализована для GroupData

--- addressbook-web-test/src/test/java/ru/stqa/homework/addressbook/model/GroupData.java
+++ addressbook-web-test/src/test/java/ru/stqa/homework/addressbook/model/GroupData.java
@@ -3,18 +3,32 @@ package ru.stqa.homework.addressbook.model;
 import com.google.gson.annotations.Expose;
 import com.thoughtworks.xstream.annotations.XStreamAlias;
 import com.thoughtworks.xstream.annotations.XStreamOmitField;
+import org.hibernate.annotations.Type;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 @XStreamAlias("group") // анотация того что сохраним в файл формата xml
-
+@Entity // анатация для того чтобы сделать привязку к БД
+@Table(name ="group_list") // делаем привязку к конкретной таблице в БД
 public class GroupData {
 
   @XStreamOmitField // анотация того что сохраним в файл формата xml
+  @Id // подсказка что это айдишник
+  @Column(name = "group_id") // тк имена атрибутов не совпадают мы должны расставить подсказки для hibernate
   private int id = Integer.MAX_VALUE;
   @Expose
+  @Column(name = "group_name") // перобразование типов происходит автоматически
   private  String name;
   @Expose
+  @Column(name = "group_header")
+  @Type(type = "text") // иногда нужно подсказать в какой тип нужно преобразовать
   private  String header;
   @Expose
+  @Column(name = "group_footer")
+  @Type(type = "text")
   private  String footer;
 
   public GroupData withHeader(String header) {
