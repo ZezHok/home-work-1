39546e0117b3854779668fb33150dd9f3e75823c
Yulia Chistova <yulia_1992@mail.ru>
2016-04-02 11:44:55 +0300

    Урок 7 Лекция 5 Отключаемые проверки UI
    Сделала для всех тестов

--- addressbook-web-test/src/test/java/ru/stqa/homework/addressbook/test/GroupModificationTests.java
+++ addressbook-web-test/src/test/java/ru/stqa/homework/addressbook/test/GroupModificationTests.java
@@ -33,6 +33,7 @@ public class GroupModificationTests extends TestBase {
     assertThat(app.group().count(), equalTo(before.size()));
     Groups after = app.db().groups();
     assertThat(after, equalTo(before.without(modifyGroup).withAdded(group)));
+    verifyGroupListInUI();
   }
 
 
--- addressbook-web-test/src/test/java/ru/stqa/homework/addressbook/test/TestBase.java
+++ addressbook-web-test/src/test/java/ru/stqa/homework/addressbook/test/TestBase.java
@@ -1,6 +1,5 @@
 package ru.stqa.homework.addressbook.test;
 
-import org.apache.bcel.classfile.Method;
 import org.openqa.selenium.remote.BrowserType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,8 +8,15 @@ import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.homework.addressbook.appmanager.ApplicationManager;
+import ru.stqa.homework.addressbook.model.ContactData;
+import ru.stqa.homework.addressbook.model.Contacts;
+import ru.stqa.homework.addressbook.model.GroupData;
+import ru.stqa.homework.addressbook.model.Groups;
 
-import java.util.Arrays;
+import java.util.stream.Collectors;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 /**
  * Created by Yulia on 29.02.2016.
@@ -26,7 +32,7 @@ public class TestBase {
     app.init();
   }
 
-  @AfterSuite ()
+  @AfterSuite()
   public void tearDown() {
     app.stop();
   }
@@ -36,9 +42,30 @@ public class TestBase {
     logger.info("Start test");
   }
 
-  @AfterMethod(alwaysRun= true)
+  @AfterMethod(alwaysRun = true)
   public void logTestStop() {
     logger.info("Stop test");
   }
 
+  public void verifyGroupListInUI() { // сравниваем инфу из бд и ui по айдишнику и имени
+    if (Boolean.getBoolean("verifyUI")) {
+      Groups dbGroups = app.db().groups();
+      Groups uiGroups = app.group().all();
+
+      assertThat(uiGroups, equalTo(dbGroups.stream()
+              .map((g) -> new GroupData().withId(g.getId()).withName(g.getName()))
+              .collect(Collectors.toSet())));
+    }
+  }
+
+  public void verifyContactListInUI() {
+    if (Boolean.getBoolean("verifyUI")) { //функция получает системное имя с данным именем и переводит его в boolean
+      Contacts dbContacts = app.db().contacts();
+      Contacts uiContacts = app.contact().all();
+
+      assertThat(uiContacts, equalTo(dbContacts.stream()
+              .map((c) -> new ContactData().withId(c.getId()).withFirstName(c.getFirstName()).withLastName(c.getLastName()))
+              .collect(Collectors.toSet())));
+    }
+  }
 }
